f64b5ea71e0c3781a3418a76a35ce8328378b49c
PawelMurawski <pawelt.murawski@gmail.com>
2019-06-25 17:43:05 +0200

    15.1

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,23 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+
+    @BeforeClass
+    public static void saveLog() {
+        Logger.getInstance().log("My first log");
+    }
+
+    @Test
+    public void testLastLog() {
+        //Given
+        //When
+        String lastLog = Logger.getInstance().getLastLog();
+        System.out.println("Last log: " + lastLog);
+        //Then
+        Assert.assertEquals("My first log", lastLog);
+    }
+}
